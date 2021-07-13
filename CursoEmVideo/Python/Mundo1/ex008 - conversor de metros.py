f0fe40d57fe6f9796a60faa7f270cdab3c7729b2
Rodrigofsiqueira <rodrigofontesdesiqueira@gmail.com>
2021-07-13 19:36:44 -0300

    Create ex008 - conversor de metros.py
    Resolução do ex008 de Python do Curso em Vídeo

---
+++ CursoEmVideo/Python/Mundo1/ex008 - conversor de metros.py
@@ -0,0 +1,9 @@
+metros = int(input('Digite um valor em metros: '))
+
+print('O valor de {} metros em'.format(metros))
+print('Kilometros é {}'.format(metros*0.0001))
+print('Hectometros é {}'.format(metros*0.001))
+print('Decametros é {}'.format(metros*0.01))
+print('Decimetros é {}'.format(metros*10))
+print('Centimetros é {}'.format(metros*100))
+print('Milimetros é {}'.format(metros*1000))
