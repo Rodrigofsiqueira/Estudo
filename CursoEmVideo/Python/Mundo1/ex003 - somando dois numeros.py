7c200d5b12bf6d820747cea103c31cd39ded213e
Rodrigofsiqueira <rodrigofontesdesiqueira@gmail.com>
2021-07-13 16:54:57 -0300

    Create ex003 - somando dois numeros.py
    Resolução do ex003 de Python do Curso em Vídeo

---
+++ CursoEmVideo/Python/Mundo1/ex003 - somando dois numeros.py
@@ -0,0 +1,5 @@
+valor1 = int(input('Digite o primeiro valor a ser somado: '))
+valor2 = int(input('Digite o segundo valor: '))
+soma = valor1 + valor2
+
+print('A soma entre {} e {} é igual a {}.'.format(valor1, valor2, soma))
