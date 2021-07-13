3d49b970139b09ad201f69b97c3a10714d5c44e5
Rodrigofsiqueira <rodrigofontesdesiqueira@gmail.com>
2021-07-13 19:50:26 -0300

    Update ex009 - tabuada.py
    Atualizei a resolução desse exercício com a estrutura de loop while, para torná-lo mais prático. Deixei a resolução antiga como comentário, no final do código.

--- CursoEmVideo/Python/Mundo1/ex009 - tabuada.py
+++ CursoEmVideo/Python/Mundo1/ex009 - tabuada.py
@@ -1,5 +1,11 @@
 n = int(input('Digite um número: '))
 
+i = 1
+while i <= 9:
+    print('{} x {} = {}'.format(n, i, n * i))
+    i += 1
+
+"""
 print('{} x 1 = {}'.format(n, n * 1))
 print('{} x 2 = {}'.format(n, n * 2))
 print('{} x 3 = {}'.format(n, n * 3))
@@ -9,3 +15,4 @@ print('{} x 6 = {}'.format(n, n * 6))
 print('{} x 7 = {}'.format(n, n * 7))
 print('{} x 8 = {}'.format(n, n * 8))
 print('{} x 9 = {}'.format(n, n * 9))
+"""
