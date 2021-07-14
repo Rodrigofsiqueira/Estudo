ed81e2b3350401e7bc8a32f51b4c037a92750af2
Rodrigofsiqueira <rodrigofontesdesiqueira@gmail.com>
2021-07-14 19:10:46 -0300

    Create ex012 - 5 por cento de desconto.py
    Resolução do ex012 de Python do Curso em Vídeo

---
+++ CursoEmVideo/Python/Mundo1/ex012 - 5 por cento de desconto.py
@@ -0,0 +1,5 @@
+valorInteiro = float(input("Digite o valor do produto desejado: "))
+
+valorDesconto = valorInteiro * 0.95
+
+print("O valor do produto, com 5% de desconto, é de {:.2f}R$.".format(valorDesconto))
