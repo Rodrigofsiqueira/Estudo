fdc297163abcd0c40fe7ee0f7ceea6d110cffe69
Rodrigofsiqueira <rodrigofontesdesiqueira@gmail.com>
2021-07-14 19:15:25 -0300

    Create ex013 - 15 por cento aumento de salario.py
    Resolução do ex013 de Python do Curso em Vídeo

---
+++ CursoEmVideo/Python/Mundo1/ex013 - 15 por cento aumento de salario.py
@@ -0,0 +1,5 @@
+salarioAtual = float(input("Digite o valor total do salário: "))
+
+salarioAumento = salarioAtual * 1.15
+
+print("O salário em questão, com 15% de aumento, vai para {:.2f}R$.".format(salarioAumento))
