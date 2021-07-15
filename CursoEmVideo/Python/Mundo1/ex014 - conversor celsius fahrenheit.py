d66a71cffa59944c5bc037ca2bb2e56f5c07d82c
Rodrigofsiqueira <rodrigofontesdesiqueira@gmail.com>
2021-07-15 14:08:13 -0300

    Create ex014 - conversor celsius fahrenheit.py
    Resolução do ex014 de Python do Curso em Vídeo

---
+++ CursoEmVideo/Python/Mundo1/ex014 - conversor celsius fahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input('Digite uma temperatura em celsius: '))
+
+fahrenheit = ((celsius * 9) / 5) + 32
+
+print('{:.1f} graus celsius correspondem a... \n{:.1f} fahrenheit'.format(celsius, fahrenheit))
